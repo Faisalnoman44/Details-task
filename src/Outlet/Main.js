ddff8fc222985835b029aecdabf912309fb8b24c
Faisalnoman44 <mdnoman.mn11@gmail.com>
2022-12-27 19:00:12 +0600

    nav added

---
+++ src/Outlet/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Nav from '../Pages/Shared/Nav/Nav';
+import Nav2 from '../Pages/Shared/Nav/Nav2';
+
+const Main = () => {
+    return (
+        <div>
+            {/* <Nav></Nav> */}
+            <Nav2></Nav2>
+        </div>
+    );
+};
+
+export default Main;
---
+++ src/Pages/Shared/Nav/Nav2.js
@@ -0,0 +1,93 @@
+import { Navbar } from 'flowbite-react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const Nav2 = () => {
+
+
+    const [theme, setTheme] = useState(
+        localStorage.getItem("theme") ? localStorage.getItem("theme") : 'system'
+    )
+    const element = document.documentElement
+    const darkQuery = window.matchMedia("(prefers-color-scheme: dark)")
+    // console.log(darkQuery, 'darkQuery')
+
+    const option = [
+        {
+            icon: 'sunny',
+            text: 'light'
+        },
+        {
+            icon: 'moon',
+            text: 'dark'
+        },
+        {
+            icon: 'desktop-outline',
+            text: 'system'
+        }
+    ]
+
+    function onWindowMatch() {
+        if (localStorage.theme === 'dark' || (!('theme' in localStorage) && darkQuery.matches)) {
+            element.classList.add('dark')
+        }
+        else {
+            element.classList.remove('dark')
+        }
+    }
+    onWindowMatch()
+
+    useEffect(() => {
+        switch (theme) {
+            case 'dark':
+                element.classList.add('dark')
+                localStorage.setItem('theme', 'dark')
+                break;
+            case 'light':
+                element.classList.remove('dark')
+                localStorage.setItem('theme', 'light')
+                break;
+
+            default:
+                localStorage.removeItem('theme')
+                onWindowMatch()
+                break;
+        }
+    }, [theme])
+
+    darkQuery.addEventListener('change', (e) => {
+        if (!("theme" in localStorage)) {
+            element.classList.add('dark')
+        }
+        else {
+            element.classList.remove('dark')
+        }
+    })
+
+
+    return (
+        <Navbar className='bg-blue-500 '>
+            <Navbar.Brand href="https://flowbite.com/">
+                <img src="https://flowbite.com/docs/images/logo.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
+                <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite</span>
+            </Navbar.Brand>
+            <div className="flex md:order-2 ">
+                {
+                    option.map(opt => <button key={opt.text} onClick={() => setTheme(opt.text)} className={`w-8 h-8 m-1 text-xl  rounded-full bg-slate-600 leading-9 ${theme === opt.text && "text-sky-600"}`}>
+                        <ion-icon name={opt.icon}></ion-icon>
+                    </button>)
+                }
+                <Navbar.Toggle />
+            </div>
+            <Navbar.Collapse className='text-white font-semibold'>
+                <Link >Home</Link>
+                <Link to=''>About</Link>
+                <Link to=''>Services</Link>
+                <Link to=''>Pricing</Link>
+                <Link to=''>Contact</Link>
+            </Navbar.Collapse>
+        </Navbar>
+    );
+};
+
+export default Nav2;
